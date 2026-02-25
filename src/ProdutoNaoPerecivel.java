7302a6f1c000dac1ca785660f6682b3d12a7046b
Arthurkkls <arthur.hls25@gmail.com>
2026-02-25 13:37:23 -0300

    fix ProdutoNaoPerecivel

--- src/ProdutoNaoPerecivel.java
+++ src/ProdutoNaoPerecivel.java
@@ -1,9 +1,7 @@
 public class ProdutoNaoPerecivel extends Produto{
     
     public ProdutoNaoPerecivel(String desc, double precoCusto, double margemLucro){
-        super.desc = desc;
-        super.precoCusto = precoCusto;
-        super.margemLucro = margemLucro;
+        super(desc, precoCusto, margemLucro);
     }
 
     @Override
