8a370bbe1c5eab5e278f18d576770047871e9e1b
Arthurkkls <arthur.hls25@gmail.com>
2026-02-25 13:45:10 -0300

    fix ProdutoPerecivel

--- src/ProdutoPerecivel.java
+++ src/ProdutoPerecivel.java
@@ -1,3 +1,23 @@
+
+import java.time.LocalDate;
+
 public class ProdutoPerecivel extends Produto{
-    
+    private static final double DESCONTO = 0.25;
+    private static final int PRAZO_DESCONTO = 7;
+    private LocalDate dataDeValidade;
+
+    public ProdutoPerecivel(String desc, double precoCusto, double margemLucro, LocalDate validade){
+        super(desc, precoCusto, margemLucro);
+        this.dataDeValidade = validade;
+    }
+
+    @Override
+    public double valorDeVenda(){
+        return super.valorDeVenda() - (super.valorDeVenda() * DESCONTO);
+    }
+
+    @Override
+    public String toString(){
+        return super.toString();
+    }
 }
